5b8c82877f7725627c0af97dac873079e37f5d47
SunilCheema <sunil03cheema@hotmail.co.uk>
2018-07-10 23:21:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-
 import os
 
 
 def iterateOverFiles(path,index):
+
     outputNumber = 1
     for filename in os.listdir(path):
         pathBeggining = path + '/'
@@ -12,15 +12,20 @@ def iterateOverFiles(path,index):
             readFile(fullPath,index,str(outputNumber))
             outputNumber += 1
 
-def readFile(input,index,outputNumber,delimeter):
+
+def readFile(input,index,outputNumber):
     with open(input) as originalFile:
+        store = []
         for i in originalFile:
-            list = i.strip().split(delimeter)
+            list = i.strip().split(" ")
             createFiles(outputNumber,list[index])
+
 
 def createFiles(number,appendedText):
     with open('/Users/sunilcheema/Documents/pyTestOutput/output' + number + '.txt', "a") as myfile:
         myfile.write(appendedText)
+    with open('/Users/sunilcheema/Documents/pyTestOutput/outputAll.txt', "a") as myfile2:
+        myfile2.write(' ' + appendedText)
 
 
 iterateOverFiles('/Users/sunilcheema/Documents/pyTest',1)
